28266305de3ac3e07d4254026aaa6a78b57e5a33
emoee <emoee@Office.uhs.ac.kr>
2024-01-03 16:23:16 +0900

    🙈 gitignore 적용 2

---
+++ src/lect/chat/client/p2p/P2P.java
@@ -0,0 +1,49 @@
+package lect.chat.client.p2p;
+import java.io.*;
+import java.net.*;
+public class P2P implements Runnable {
+	ServerSocket ss;
+	public static int PORT = 1224;
+	private static P2P instance;
+	
+	private P2P () {}
+	
+	public static P2P getInstance() {
+		if(instance == null) instance = new P2P();
+		return instance;
+		// 싱글톤 패턴
+	}
+	
+	public void startService() throws IOException {
+		if(ss != null && ss.isBound()) return;
+		ss = new ServerSocket(PORT);
+		new Thread(this).start();
+		// 소켓없으면 생성, 스레드 생성해 호출
+	}
+	
+	public void stopService() {
+		try {
+			ss.close();
+			ss = null;
+		} catch(IOException e) {
+			// 소켓 닫기
+		}
+	}
+	
+	@Override
+	public void run () {
+		Socket s;
+		try {
+			while(true) {
+				s = ss.accept();
+				new Thread(new FileReceiver(s)).start();
+			}
+			// 소켓 연결 받아드려 스레드 생성해 파일 보내기 시작
+		} catch(IOException e) {
+			
+		} finally {
+			try { if(ss != null) ss.close(); } catch(Exception e) {}
+		}
+	}
+
+}
---
+++ src/lect/chat/server/GroupManager.java
@@ -0,0 +1,80 @@
+package lect.chat.server;
+import java.util.*;
+
+import lect.chat.protocol.ChatCommandUtil;
+public class GroupManager {
+	private static Vector<MessageHandler> clientGroup = new Vector<MessageHandler>();
+	private GroupManager() {} // 객체 생성 방지로 private 선언
+	public static void addMessageHandler(MessageHandler handler) {
+		// 새로운 사용자 그룹에 추가. ClientHandler에서 호출됨
+		//broadcastMessage(handler.getId() + " has just entered chat room");
+		clientGroup.add(handler);
+		System.out.println("Active clients count: " + clientGroup.size());
+	}
+	public static void removeMessageHandler(MessageHandler handler) {
+		// 사용자 제거
+		clientGroup.remove(handler);
+		System.out.println("Active clients count: " + clientGroup.size());
+		// 퇴장 알림
+		for(MessageHandler mh: clientGroup) {
+			mh.sendMessage(createMessage(ChatCommandUtil.EXIT_ROOM, handler.getName() + " has just left chat room"));
+		}
+	}
+	public static void broadcastMessageAllClient(String msg) {
+		// 모든 사용자에게 메세지 전송
+		for(MessageHandler handler: clientGroup) {
+			handler.sendMessage(createMessage(ChatCommandUtil.NORMAL, msg));
+		}
+	}
+	public static void closeAllMessageHandlers() {
+		// 서버 정리 시 모든 사용자의 메세지 핸들러 종료
+		for(MessageHandler handler: clientGroup) {
+			handler.close();
+		}
+		clientGroup.clear();
+	}
+	public static void broadcastNewChatterAllClient(MessageHandler newHandler) {
+		// 새로운 사용자 입장시 모든 사용자에게 알림
+		StringBuilder users = new StringBuilder();
+		MessageHandler handler;
+		int size = clientGroup.size();
+		for(int i = 0; i < size; i++) {
+			handler = clientGroup.get(i);
+			users.append(handler.getName()).append(",")
+			.append(handler.getId()).append(",")
+			.append(handler.getFrom());
+			if(i < (size-1)) {
+				users.append("|");
+			}
+		}
+		for(MessageHandler mh: clientGroup) {
+			if(mh != newHandler) {
+				mh.sendMessage(createMessage(ChatCommandUtil.ENTER_ROOM, (newHandler.getName() + " has just entered chat room")));
+			}
+			mh.sendMessage(createMessage(ChatCommandUtil.USER_LIST, users.toString()));
+		}
+	}
+	public static void sendWhisper(MessageHandler from, String to, String msg) {
+		// 귓속말 전송, 보낸사람 받는 사람 둘에게만
+		msg = createMessage(ChatCommandUtil.WHISPER, msg);
+		for(MessageHandler handler: clientGroup) {
+			if(handler.getId().equals(to)) { // 선택된 사용자 찾아서 해당 사용자에게만 메세지 전송
+				handler.sendMessage(msg);
+				break;
+			}
+		}
+		// 보낸 사용자에게도 전송
+		from.sendMessage(msg);
+	}
+	static String createMessage(char command, String msg) {
+		// 메세지 형식에 따라 메세지 생성
+		StringBuilder msgBuilder = new StringBuilder();
+		msgBuilder.delete(0, msgBuilder.length());
+		msgBuilder.append("[");
+		msgBuilder.append(command);
+		msgBuilder.append("]");
+		msgBuilder.append(msg);
+		return msgBuilder.toString();
+		
+	}
+}
